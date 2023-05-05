d83239e480b4bb4ef76c0214405d71fa2006f836
Jim Martin <kelleymartin872@gmail.com>
2023-05-05 16:38:43 -0700

    added clock

--- frontend/ico-dapp-ui/src/App.js
+++ frontend/ico-dapp-ui/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Clock from "./components/Clock.js"
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <Clock />
+      </div>
     </div>
   );
 }
---
+++ frontend/ico-dapp-ui/src/components/Clock.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const gmtTime = time.toUTCString();
+
+  return (
+    <div>
+      <h1>Current Time: {gmtTime} </h1>
+      
+    </div>
+  );
+}
+
+export default Clock;
