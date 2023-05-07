dd312735443ac358ca2e2b48c8c2b71d568142b7
Jim Martin <kelleymartin872@gmail.com>
2023-05-06 17:42:28 -0700

    ui complete

--- frontend/ico-dapp-ui/src/App.js
+++ frontend/ico-dapp-ui/src/App.js
@@ -1,29 +1,39 @@
 
 import './App.css';
 import Clock from "./components/Clock.js"
-import InputForm from './components/InputForm';
+import Deposit from "./components/Deposit.js"
+
+import React, { useState, useEffect } from 'react';
+import ProgressBar from "./components/ProgressBar";
+// import InputForm from './components/InputForm';
 
 
 function App() {
-  return (
-      <div className="App">
-        <header>
 
-        </header>
-        asdfadf
-        <div className="row work">
-          <div className='three columns header-col'>
-            <Clock />
-          </div>
-          <div className="six columns header-col"></div>
-          <div className="three columns header-col">
-            <a href = " " target = '_blank'><h3>Connect Wallet</h3></a>
-          </div>
+  return (
+    <div className="App">
+      <header />
+      <div className="row work">
+        <div className='three columns header-col'>
+          <Clock />
         </div>
-        <div className = "row">
-          <InputForm/>
+        <div className="six columns header-col"></div>
+        <div className="three columns header-col">
+          <a href=" " target='_blank'><h3>Connect Wallet</h3></a>
         </div>
       </div>
+      <div className="row work">
+        <Deposit buttontext="Deposit" />
+      </div>
+      <div className="row work">
+        <ProgressBar />
+      </div>
+      <div className='row'>
+        <div className="four columns header-col"> <h3>Start Time :2023/05/08 00:00:00 GMT</h3> </div>
+        <div className="four columns header-col"> </div>
+        <div className="four columns header-col"><h3>End Time :2023/05/09 00:00:00 GMT</h3></div>
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/ico-dapp-ui/src/components/ProgressBar.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import { Progress } from 'antd';
+
+function ProgressBar(props) {
+
+    const [value, setValue] = useState(10);
+    const [softcap, setSoftcap] = useState(10);
+    const [hardcap, setHardcap] = useState(100);
+
+
+    const getColor = () => {
+        if (value < softcap) {
+            return '#ff0000';
+        } else if (value >= softcap && value < hardcap) {
+            return '#00ff00';
+        } else {
+            return '#0000ff';
+        }
+    };
+
+
+    return (
+        <div>
+
+            <div className='twelve columns'>
+                <Progress
+                    percent={value}
+                    strokeColor={getColor()}
+                    trailColor='gray'
+                    strokeWidth={20}
+                    showInfo={false}
+                    status="active"
+                />
+                <div style={{ marginTop: 10 }}>
+                    <span style={{ float: 'left' }}>0 BMB</span>
+                    <span style={{ float: 'right' }}>1 BNB</span>
+                    <div style={{ float: 'left', position: 'relative', width: '100%' }}>
+                        <span style={{ position: 'absolute', left: `${softcap}%`, top: -80 }}>
+                            <span style={{ borderLeft: '1px solid #ccc', height: 12, display: 'inline-block', position: 'absolute', left: -1 }}></span>
+                            <span style={{ backgroundColor: '#ccc', borderRadius: '50%', width: 10, height: 10, display: 'inline-block', position: 'absolute', left: -5, top: -4 }}></span>
+                            <span style={{ position: 'absolute', left: -10, top: -30 }}>SoftCap</span>
+                        </span>
+                        <span style={{ position: 'absolute', left: `${hardcap}%`, top: -80 }}>
+                            <span style={{ borderLeft: '1px solid #ccc', height: 12, display: 'inline-block', position: 'absolute', left: -1 }}></span>
+                            <span style={{ backgroundColor: '#ccc', borderRadius: '50%', width: 10, height: 10, display: 'inline-block', position: 'absolute', left: -5, top: -4 }}></span>
+                            <span style={{ position: 'absolute', left: -10, top: -30 }}>HardCap</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default ProgressBar;
