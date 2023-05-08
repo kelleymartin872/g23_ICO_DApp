972aec6e22ac460e0bbdfdde1a033759c2f88b4c
ds-astroboy <davidsanter397@gmail.com>
2023-05-08 13:16:16 -0700

    update: all

---
+++ .history/contracts/ICO_20230508131601.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./ICOToken.sol";
+
+contract ICO {
+    ICOToken public token;
+    address payable public owner;
+    uint256 public softCap = 0.1 ether;
+    uint256 public hardCap = 1 ether;
+    uint256 public totalEtherRaised = 0;
+    uint256 public startTime = 1654060800; // January 1, 2022 12:00:00 AM GMT
+    uint256 public endTime = 1654464000; // January 5, 2022 12:00:00 AM GMT
+
+    uint256 public constant MIN_PURCHASE_AMOUNT = 0.01 ether;
+    uint256 public constant MAX_PURCHASE_AMOUNT = 0.05 ether;
+
+    // Deposits Mapping
+    mapping(address => uint256) public deposits;
+
+    constructor(address tokenAddress) {
+        token = ICOToken(tokenAddress);
+        owner = payable(msg.sender);
+    }
+
+    // purchase tokens from ICO contract = Deposit
+    receive() external payable {
+        require(msg.sender != address(0), "Invalid address");
+        require(totalEtherRaised < hardCap, "Hard cap reached");
+        // During ICO
+        require(block.timestamp >= startTime, "ICO has not started yet");
+        require(block.timestamp <= endTime, "ICO has ended");
+        // MIN/MAX Purchase Amount
+        require(msg.value >= MIN_PURCHASE_AMOUNT, "Purchase amount too small");
+        require(msg.value <= MAX_PURCHASE_AMOUNT, "Purchase amount too large");
+
+        uint256 etherAmount = msg.value;
+        deposits[msg.sender] += etherAmount;
+        totalEtherRaised += etherAmount;
+    }
+
+    function withdraw() external {
+        require(msg.sender != address(0), "Invalid address");
+        require(block.timestamp > endTime, "ICO has not ended yet");
+        require(totalEtherRaised < softCap, "Soft cap has been reached");
+
+        uint256 depositAmount = deposits[msg.sender];
+        deposits[msg.sender] = 0;
+        payable(msg.sender).transfer(depositAmount);
+    }
+
+    function claim() external {
+        require(msg.sender != address(0), "Invalid address");
+        require(block.timestamp > endTime, "ICO has not ended yet");
+        require(totalEtherRaised >= softCap, "Soft cap has not been reached");
+
+        uint256 etherAmount = deposits[msg.sender];
+        require(etherAmount > 0, "You don't have any tokens to claim");
+        uint256 tokenAmount = etherAmount * 1000;
+        deposits[msg.sender] = 0;
+        
+        // token.transfer(msg.sender, tokenAmount);
+        token.mint(msg.sender, tokenAmount);
+        // emit TokensClaimed(msg.sender, tokenAmount);
+    }
+}
--- .history/contracts/Token_20230505144944.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.10;
-import "./ERC20/ERC20.sol"
-
-contract Token {
-    
-}
